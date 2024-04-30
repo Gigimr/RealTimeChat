c365969573c88dc7780a692bf1ec0fe061dbbdaa
Gigi <gigi151521@gmail.com>
2024-04-30 15:31:19 -0600

    Created socket server

--- backEnd/index.js
+++ backEnd/index.js
@@ -3,9 +3,28 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const cors = require('cors');
+const { Server } = require('socket.io');
 
 app.use(cors());
 
 const server = http.createServer(app);
 
-server.listen(3001, () => {});
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+
+    socket.on('disconnect', () => {
+        console.log('A user disconnected', socket.id);
+    });
+});
+
+
+server.listen(3001, () => {
+    console.log('Server is running on port 3001');
+});
