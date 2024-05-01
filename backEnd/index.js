eb313118d1fa504ccee17d4f2c3b31f860c9ed0f
Gigi <gigi151521@gmail.com>
2024-05-01 09:38:06 -0600

    Update chat functionality and socket.io configuration

--- backEnd/index.js
+++ backEnd/index.js
@@ -25,7 +25,7 @@ io.on('connection', (socket) => {
     })
     
     socket.on('send_message', (data) => {
-       console.log('Mensabje recibido', data);
+      socket.to(data.room).emit('receive_message', data);
     })
     socket.on('disconnect', () => {
         console.log('A user disconnected', socket.id);
--- frontEnd/src/components/Chat.jsx
+++ frontEnd/src/components/Chat.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import  { useEffect, useState } from "react";
 
 const Chat = ({ socket, userName, room}) => {
 
@@ -17,6 +17,12 @@ const Chat = ({ socket, userName, room}) => {
             await socket.emit('send_message', info);
         }
     }
+    useEffect(()=>{
+        socket.on('receive_message', (data) => {
+            console.log(data);
+        })
+    },[socket])
+
   return (
   <div>
     <section className="chat-header">
